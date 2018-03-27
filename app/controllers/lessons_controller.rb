437fe6cdfcb3cdf6a9758643bd0155e85c9a2527
dannyb21892 <dannyb21892@verizon.net>
2018-03-27 18:06:02 -0400

    Lessons index and show under construction

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,2 +1,17 @@
 class LessonsController < ApplicationController
+  def index
+    params[:open_select] ? is_open = params[:open_select] : is_open = nil
+    ["", nil].include?(params[:trainer_select]) ? trainer = Trainer.find(params[:trainer_select]) : trainer = nil
+    @lessons = Lesson.all
+    if is_open == "1"
+      @lessons = @lessons.select{|l| !l.full?}
+    end
+    if trainer
+      @lessons = @lessons.select{|l| l.trainer == trainer}
+    end
+  end
+
+  def show
+    @lesson = Lesson.find(params[:id])
+  end
 end
