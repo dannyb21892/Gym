a172880189f248e52ddd07618d84a7f1aa0d8a17
vsparrow <khan@vsparrow.com>
2018-03-28 10:13:59 -0400

    trainers views added

--- app/controllers/trainers_controller.rb
+++ app/controllers/trainers_controller.rb
@@ -1,2 +1,11 @@
 class TrainersController < ApplicationController
+  def index
+    @trainers = Trainer.all
+  end
+
+  def show
+    @trainer=Trainer.find(params[:id])
+    @image = @trainer.name.downcase.split(" ").join("-") << ".jpg"
+    STDERR.puts "****************#{@image}"
+  end
 end
---
+++ app/ftest.rb
@@ -0,0 +1,8 @@
+require 'faker'
+puts Faker::Name.name   
+puts Faker::DragonBall.character
+puts Faker::Simpsons.character
+
+
+
+puts User.all
