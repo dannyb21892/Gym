e59f37877bd5055c2d8edd7ebe502398eec13ad0
dannyb21892 <dannyb21892@verizon.net>
2018-03-29 15:21:58 -0400

    admin user status implemented with admin-only analytics page

--- app/models/trainer.rb
+++ app/models/trainer.rb
@@ -1,5 +1,6 @@
 class Trainer < ApplicationRecord
   has_many :lessons
+  has_many :users, through: :lessons
 
   def students
     self.lessons.map{|l| l.users.map{|u| u.name}}.flatten.uniq
@@ -7,6 +8,19 @@ class Trainer < ApplicationRecord
 
   def all_class_roster_size
     self.lessons.map{|l| l.users.map{|u| u.name}}.flatten.count
+  end
+
+  def self.sorted_popularity
+    Trainer.all.map{|t| [t, t.all_class_roster_size]}.sort_by{|i| i[1]}.reverse!
+  end
+
+  def trainer_revenue
+    total = 0
+    self.users.each do |user|
+      relevant_lessons = user.lessons.select{|l| l.trainer == self}
+      total += (user.plan.price.to_f / user.lessons.count)*relevant_lessons.count
+    end
+    total
   end
 
 end
